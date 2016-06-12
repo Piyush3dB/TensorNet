3b7d8db7ffff03064b0f6b3a9f17bbeeb4c759b9
Piyush Singh <mrsinghpiyush@gmail.com>
2016-06-12 20:55:06 +0100

    Add paths

--- addpaths.m
+++ addpaths.m
@@ -3,6 +3,11 @@ here = pwd;
 disp 'Add matconvnet'
 cd matconvnet/matlab
 run vl_setupnn
+
+cd(here);
+addpath(fullfile(pwd, 'matconvnet/examples'));
+addpath(fullfile(pwd, 'src/matlab'));
+
 cd(here);
 
 disp 'Add TT-Toolbox'
--- runMnist.m
+++ runMnist.m
@@ -6,7 +6,7 @@ format compact;
 
 here = pwd;
 disp 'Run mnist experiment...'
-
+addpath(fullfile(pwd, 'matconvnet/examples'));
 cd experiments/mnist
 
 [net_tt, info_tt] = cnn_mnist_tt('expDir', 'data/mnist-tt');
